abfbaaa7ae530e5bf7f2d3beec4c85589d1ae5ab
mohamedBA <mohamedbenabdallah92@gmail.com>
2015-11-04 21:52:12 +0100

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include "mainwindow.h"
+#include "ui_mainwindow.h"
+#include <QApplication>
+#include<QDebug>
+#include<QPixmap>
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    return a.exec();
+}
